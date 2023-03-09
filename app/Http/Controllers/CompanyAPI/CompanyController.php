c0b466e692aa066680abf00a4ab02679eff9d621
Danward Bryle Bautista <danwarddeveloper@gmail.com>
2023-03-09 12:22:44 +0800

    Company API
    - CRUD for companies table
    - Added POST and PUT validations
    - Added image auto delete on PUT method
    - Added migration for companies table
    - Added routes for company API

---
+++ app/Http/Controllers/CompanyAPI/CompanyController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Controllers\CompanyAPI;
+
+use App\Http\Controllers\Controller;
+use App\Models\Companies;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CompanyController extends Controller
+{
+    //READ ALL COMPANY
+    public function index()
+    {
+        $companies = Companies::get();
+
+        $status_code = 200;
+
+        return response([
+            'message' => "Companies displayed successfully",
+            'status' => 'OK',
+            'status_code' => $status_code,
+            'results' => $companies
+        ], $status_code);
+    }
+
+    //CREATE COMPANY
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|unique:companies',
+            'email' => 'nullable|email|unique:companies',
+            'logo' => 'nullable|mimes:jpeg,jpg,png|dimensions:min_width=100,min_height=100',
+            'website' => 'nullable|url',
+        ]
+        ;
+        $validator = Validator::make($request->all(), $rules, $messages= [
+            'logo.dimensions' => 'Minimum company logo size is 100x100.',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $data = $request->validate([
+            'name' => 'required|unique:companies',
+            'email' => 'nullable',
+            'website' => 'nullable',
+        ]);
+
+        $companies = Companies::create($data);
+        $status_code = 201;
+
+        if ($request->hasFile('logo')) {
+
+            $uploaded_files = $request->logo->store('public/CompanyLogo/');
+            $companies->logo = $request->logo->hashName();
+
+            $results = $companies->save();
+        }
+
+        return response([
+            'message' => "Company created successfully", 
+            'status' => 'CREATED',
+            'status_code' => $status_code,
+            'results' => $companies
+        ], $status_code);
+    }
+
+    //READ ALL COMPANY BY ID
+    public function show($id)
+    {
+        $companies = Companies::find($id);
+
+        if (!$companies) {
+
+            $error_status_code = 404;
+
+            abort(
+                response()->json([
+                    'message' => "Company not found!",
+                    'status' => 'NOT FOUND',
+                    'status_code' => $error_status_code,
+                    'results' => []
+                ], $error_status_code)
+            );
+        }
+
+        $status_code = 200;
+
+        return response([
+            'message' => "Company displayed successfully",
+            'status' => 'OK',
+            'status_code' => $status_code,
+            'results' => $companies
+        ], $status_code);
+    }
+
+    //UPDATE COMPANY BY ID
+    public function update(Request $request, $id)
+    {
+        $companies = Companies::find($id);
+        if (!$companies) {
+
+            $error_status_code = 404;
+
+            abort(
+                response()->json([
+                    'message' => "Company not found!",
+                    'status' => 'NOT FOUND',
+                    'status_code' => $error_status_code,
+                    'results' => []
+                ], $error_status_code)
+            );
+        }
+
+        $rules = [
+            'name' => 'required|unique:companies,name,' . $companies->id,
+            'email' => 'nullable|email|unique:companies,email,' . $companies->id,
+            'logo' => 'nullable|mimes:jpeg,jpg,png|dimensions:min_width=100,min_height=100',
+            'website' => 'nullable|url',
+        ]
+        ;
+        $validator = Validator::make($request->all(), $rules, $messages= [
+            'logo.dimensions' => 'Minimum company logo size is 100x100.',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'nullable',
+            'website' => 'nullable',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            if ($companies->logo != null) {
+                if (file_exists(storage_path('app/public/CompanyLogo/' . $companies->logo))) {
+                    unlink(storage_path('app/public/CompanyLogo/' . $companies->logo));
+                }
+            }
+
+            $uploaded_files = $request->logo->store('public/CompanyLogo/');
+            $companies->logo = $request->logo->hashName();
+
+            $results = $companies->save();
+        }
+
+        $companies->update($data);
+        $status_code = 200;
+
+        return response([
+            'message' => "Company updated successfully", 
+            'status' => 'OK',
+            'status_code' => $status_code,
+            'results' => $companies
+        ], $status_code);
+    }
+
+    //DELETE Company
+    public function destroy($id)
+    {
+        $companies = Companies::find($id);
+
+        if (!$companies) {
+            $error_status_code = 404;
+
+            abort(
+                response()->json([
+                    'message' => "Company not found!",
+                    'status' => 'NOT FOUND',
+                    'status_code' => $error_status_code,
+                    'results' => []
+                ], $error_status_code)
+            );
+        }
+        if ($companies->logo != null) {
+            unlink(storage_path('app/public/CompanyLogo/' . $companies->logo));
+        }
+
+        $companies->delete();
+        $status_code = 200;
+
+        return response([
+            'message' => "Company deleted successfully", 
+            'status' => 'OK',
+            'status_code' => $status_code,
+            'results' => $companies
+        ], $status_code);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyAPI\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/company', [CompanyController::class, 'index']);
+Route::post('/company', [CompanyController::class, 'store']);
+Route::get('/company/{id}', [CompanyController::class, 'show']);
+Route::put('/company/{id}', [CompanyController::class, 'update']);
+Route::delete('/company/{id}', [CompanyController::class, 'destroy']);
